da0762fd473e6103e8a1a8a5fc20772868e78dfe
root <danielwanderi70>
2023-08-16 11:08:39 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
 void print_alphabet_x10(void)
 {
 char alphabet;
-int i;
-for (i=0;i<10;i++)
+int i ;
+for (i = 0 ; i < 10 ; i++)
 {
-for (alphabet='a';alphabet<='z';alphabet++)
+for (alphabet= 'a' ; alphabet <= 'z' ; alphabet++)
 {
-_putchar(alphabet);
+_putchar(alphabet) ;
 }
-_putchar('\n');
+_putchar('\n') ;
 }
 }
 
