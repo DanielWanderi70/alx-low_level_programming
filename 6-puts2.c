fc4c5ba020cf47fd2028d384c7e51543f7d82280
DanielWanderi70 <133268198+DanielWanderi70@users.noreply.github.com>
2023-08-22 19:16:18 +0300

    Half the lies they tell about me aren't true

---
+++ 6-puts2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * put2 function prints every other character in a string
+*/
+void puts2(char *str)
+{
+  int n
+for(n = 0; str[n] !? "/0"; n++);
+{
+_putchar(str[n]);
+}
+}
