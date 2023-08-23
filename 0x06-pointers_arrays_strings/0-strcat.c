6056489b2f75124ecab21e934dc45872251a0707
root <danielwanderi70>
2023-08-23 02:35:35 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * function appends two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (j = 0; scr[j] != '\0'; j++)
+		{
+			_putchar(i j);
+		}
+	}
+}
+
