5fe0957c18809acce970fedb980d762a9e06d046
root <danielwanderi70>
2023-08-24 13:26:08 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * function that capitilizes every other word
+ */
+char *cap_string(char *)
+{
+        int i;
+	if (char[i] >= 'a' && char[i] <= 'z')
+	{
+		char[i] = char[i] - 32;
+	}
+        for (i = 0; char[i] = '\0'; i++)
+        {       
+                if (char[i] = 32 || char[i] = 44 || char[i] = 59 || char[i] = 46 || char[i] = 33 || char[i] = 63 || char[i] = 34 || char[i] = 40 || char[i] = 41 || char[i] = 123 || char[i] = 125 || char[i] = 9)
+                {       
+                        char[i] = char[i] - 32;
+		}
+		return (char);
+	}
+}
