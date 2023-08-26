057d9aafe46216321ad07293276887efd1a06a63
DanielWanderi70 <133268198+DanielWanderi70@users.noreply.github.com>
2023-08-26 14:01:48 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes  master

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *)
 	}
         for (i = 0; char[i] != '\0'; i++)
         {       
-                if (char[i] = 32 || char[i] = 44 || char[i] = 59 || char[i] = 46 || char[i] = 33 || char[i] = 63 || char[i] = 34 || char[i] = 40 || char[i] = 41 || char[i] = 123 || char[i] = 125 || char[i] = 9)
+                if (char[i] == 32 || char[i] == 44 || char[i] == 59 || char[i] == 46 || char[i] == 33 || char[i] == 63 || char[i] == 34 || char[i] == 40 || char[i] == 41 || char[i] == 123 || char[i] == 125 || char[i] == 9)
                 {       
                         char[i] = char[i] - 32;
 		}
