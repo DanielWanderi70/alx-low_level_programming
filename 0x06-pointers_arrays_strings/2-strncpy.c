1234debeda7dbed3a9f6fb1d9b9928f478f77755
DanielWanderi70 <133268198+DanielWanderi70@users.noreply.github.com>
2023-08-27 15:23:10 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,17 +5,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	for (i = 0; i < n && i != '\0'; i++)
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		scr[i] = dest[i];
+		dest[i] = src[i];
 	}
-	while (i < n)
-	{
+	for (; i < n; i++)
 		dest[i] = '\0';
-
-
-	}
 	return (dest);
 }
+	
+
 
 
