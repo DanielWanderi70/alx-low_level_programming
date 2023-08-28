3ffa1e16926bf017600e6caaf5d205c9daf07f8d
root <danielwanderi70>
2023-08-28 08:37:03 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * function shares memory space
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[n];
+	}
+	return (dest);
+}
+
