a9807ef99947d12d6e8739b5c3145f62fa7e1177
root <danielwanderi70>
2023-08-30 08:25:10 -0700

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	static int i = 0;
-	if (s[i] != '\0')
+	if (s* != '\0')
 	{
-	_putchar(s[i]);
-        i++;
-	_puts_recursion(s);
+	_putchar(s*);
+	_puts_recursion(s + 1);
 	}
 	else
 	{
