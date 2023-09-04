b0a05bf9bad8516eba79f24d9956439d8468bcf9
root <danielwanderi70>
2023-09-04 04:28:05 -0700

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * description create_array
+ * @size: name of variable
+ * @c: inputs character c
+ * returns a null
+ */
+char *create_array(unsigned int size, char c)
+{
+}
