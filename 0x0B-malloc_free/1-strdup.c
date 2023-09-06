b575d2db44722f3a30c85a42adca97116c34e35a
DanielWanderi70 <133268198+DanielWanderi70@users.noreply.github.com>
2023-09-06 21:12:04 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,20 +6,27 @@
  * return a char
  *
  */
-char *_strdup(char *str)
-{       
-        int i = 0;
-	
-        char  *size;
-        if (str == 0)
-        {       
-                return (0);
-        }       
-      size = (char *)malloc((i + 1) * sizeof(char));
-       
-		for (; str[i] != '\0'; i++)
-		str[i] = size[i];
-		return (size);
-		
 
+char *_strdup(char *str)
+{
+int i = 0, size = 0;
+char *m;
+
+if (str == NULL)
+	return (NULL);
+
+for (; str[size] != '\0'; size++)
+;
+
+/*+1 on the size puts the end of string character*/
+m = malloc(size * sizeof(*str) + 1);
+
+if (m == 0)
+	return (NULL);
+else
+{
+	for (; i < size; i++)
+		m[i] = str[i];
+}
+return (m);
 }
