c2e75748ab14e6901f4ea8e0b3f2dbf2138ddc2c
root <danielwanderi70>
2023-09-12 08:42:42 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,27 @@
-#include "main.h"
-/*
- * description structure dog
- * @name: char
- * @age : int
- * @owner: char
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
